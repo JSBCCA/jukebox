dde3f15eb32058dcf2215ada0ee72b7757cb1508
James Sibert <jsibert@basecampcodingacademy.org>
2016-12-20 14:31:36 -0600

    added icon, song, and fixed js

--- juke.js
+++ juke.js
@@ -1,1 +1,23 @@
-// play sound on key push?
+songlist = [["Megalovania", "megalovania"],
+            ["Spider Dance", "spider_dance"],
+            ["Death by Glamour", "death_by_glamour"]]
+
+var current_item = 0;
+
+function change_track(song_info) {
+  $("#title").text(song_info[0]);
+  $("#songsource").attr("src", "song/" + song_info[1] + ".mp3");
+}
+
+$("#button2").on("click", function() {
+    change_track(songlist[current_item + 1]);
+    current_item = (current_item + 1) % songlist.length;
+  });
+
+$("#button1").on("click", function() {
+  change_track(songlist[current_item - 1]);
+  current_item = (current_item - 1);
+  if (current_item < 0) {
+    current_item = songlist.length - 1;
+  };
+});
