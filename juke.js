5a1e141567cdc6eec73d031edc333b61a9391113
James Sibert <jsibert@basecampcodingacademy.org>
2016-12-22 14:44:48 -0600

    removed controls, pauseplay works

--- juke.js
+++ juke.js
@@ -15,21 +15,31 @@ function change_track(song_info) {
     audio.load();
 };
 
+function pauseplay() {
+  $('#pause').css('display', 'none');
+  $('#play').css('display', 'inline-block');
+};
+
 $("#button2").on("click", function() {
     current_item = (current_item + 1) % songlist.length;
     change_track(songlist[current_item]);
+    pauseplay();
 });
 
 $("#button1").on("click", function() {
     current_item = (current_item - 1);
     if (current_item < 0) {current_item = songlist.length - 1;};
     change_track(songlist[current_item]);
+    pauseplay();
 });
 
 $("#play").on("click", function() {
-
+    audio.play();
+    $('#play').css('display', 'none');
+    $('#pause').css('display', 'inline-block');
 });
 
 $("#pause").on("click", function() {
-
+    audio.pause();
+    pauseplay();
 });
