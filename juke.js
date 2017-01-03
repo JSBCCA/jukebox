8f5f584f1bfcaadef86a61307ad36e665ff5d0ce
James Sibert <jsibert@basecampcodingacademy.org>
2017-01-03 10:33:55 -0600

    added 6 songs

--- juke.js
+++ juke.js
@@ -1,19 +1,25 @@
 songlist = [["Gusty Garden Galaxy", "gusty", "Super Mario Galaxy"],
+            ["Title Theme", "title", "Ocarina of Time"],
             ["Song of Storms", "storms", "Ocarina of Time"],
             ["Gerudo Valley", "gerudo", "Ocarina of Time"],
             ["Song of Healing", "healing", "Majora's Mask"],
+            ["The Great Sea", "greatsea", "The Wind Waker"],
             ["Hyrule Field", "hyrule_field", "Twilight Princess"],
             ["Midna's Lament", "midna_lament", "Twilight Princess"],
-            ["Halo Main Theme", "halo", "Halo"],
-            ["Jingle Jangle Jingle", "jingle", "Fallout New Vegas"],
-            ["Butcher Pete", "butcher_pete", "Fallout 3"],
+            ["Zero Two", "zerotwo", "Kirby 64: The Crystal Shards"],
+            ["Dr. Wily's Castle Stage 1", "wily", "Mega Man 2"],
             ["Simple and Clean", "simple", "Kingdom Hearts"],
             ["Guardando nel Buio", "ansem", "Kingdom Hearts"],
             ["Sanctuary", "sanctuary", "Kingdom Hearts 2"],
+            ["Roxas' Theme", "roxas", "Kingdom Hearts 2"],
             ["Sacred Moon", "sacred_moon", "Kingdom Hearts 2"],
             ["Simple and Clean (Ray of Hope Mix)", "rayofhope", "Kingdom Hearts 0.2"],
+            ["One Winged Angel", "onewinged", "Final Fantasy 7"],
             ["Apocalypsis Aquarius", "aquarius", "Final Fantasy 15"],
             ["Somnus", "somnus", "Final Fantasy 15"],
+            ["Halo Main Theme", "halo", "Halo"],
+            ["Jingle Jangle Jingle", "jingle", "Fallout New Vegas"],
+            ["Butcher Pete", "butcher_pete", "Fallout 3"],
             ["Undertale", "undertale", "Undertale"],
             ["Heartache", "heartache", "Undertale"],
             ["sans.", "sans", "Undertale"],
@@ -29,17 +35,12 @@ songlist = [["Gusty Garden Galaxy", "gusty", "Super Mario Galaxy"],
             ["Hexagon", "hexagon", "Super Hexagon"],
             ["Main Theme", "batman", "Batman Arkham City"]]
             // play while looking through songs
-            // more songs
-            // look though games
+            // search by game
 
-
-            // ocarina opening
-            // final fantasy 7 main theme, one winged angel
-            // kirby zero two
-            // mega man 2 dr. wily's castle
-            // wind waker great sea, dragon roost
-            // open your heart, live and learn, perfect dark gaia
-            // roxas theme?
+            // dragon roost
+            // open your heart
+            // live and learn
+            // perfect dark gaia
             // zeldas lullaby
 
 
@@ -72,12 +73,75 @@ $("#button1").on("click", function() {
 });
 
 $("#play").on("click", function() {
+    // if (audio.duration > 0 && !audio.paused) {
+    //   audio.pause();
+    //   audio.load();
+    // };
     audio.play();
     $('#play').css('display', 'none');
     $('#pause').css('display', 'inline-block');
 });
 
+// if (myAudio.duration > 0 && !myAudio.paused) {
+
 $("#pause").on("click", function() {
     audio.pause();
     pauseplay();
 });
+
+
+
+//
+// var current_item = 0;
+// var placehold = 0;
+//
+// function change_track(song_info) {
+//     // audio.pause();
+//     $("#title").text(song_info[2]);
+//     $("#music").text(song_info[0]);
+//     $("#songsource").attr("src", "song/" + song_info[1] + ".mp3");
+//     // audio.load();
+// };
+//
+// function pauseplay() {
+//   $('#pause').css('display', 'none');
+//   $('#play').css('display', 'inline-block');
+// };
+//
+// $("#button2").on("click", function() {
+//     if ($('pause').css('display') == 'inline-block') {placehold = current_item;};
+//     current_item = (current_item + 1) % songlist.length;
+//     change_track(songlist[current_item]);
+//     pauseplay();
+//     if (current_item == placehold) {
+//       $('#play').css('display', 'none');
+//       $('#pause').css('display', 'inline-block')
+//     ;}
+// });
+//
+// $("#button1").on("click", function() {
+//     if ($('pause').css('display') == 'inline-block') {placehold = current_item;};
+//     current_item = (current_item - 1);
+//     if (current_item < 0) {current_item = songlist.length - 1;};
+//     change_track(songlist[current_item]);
+//     pauseplay();
+//     if (current_item == placehold) {
+//       $('#play').css('display', 'none');
+//       $('#pause').css('display', 'inline-block')
+//     ;}
+// });
+//
+// $("#play").on("click", function() {
+//     if (audio.duration > 0 && !audio.paused) {
+//       audio.pause();
+//       audio.load();
+//     };
+//     audio.play();
+//     $('#play').css('display', 'none');
+//     $('#pause').css('display', 'inline-block');
+// });
+//
+// $("#pause").on("click", function() {
+//     audio.pause();
+//     pauseplay();
+// });
