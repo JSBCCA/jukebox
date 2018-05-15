05561533cbb4b8ecc830b3262d0d513e64b049f7
JamesS <jsibert@basecampcodingacademy.org>
2018-05-14 19:45:38 -0500

    Noted autocomplete for testing

--- juke.js
+++ juke.js
@@ -269,7 +269,7 @@ var song_searchbar = {
 
 var current_item = 0;
 var placehold = null;
-var gamenames = [for (s of songlist) for (i of s) if (s.indexOf(i) === 2) i];
+// var gamenames = [for (s of songlist) for (i of s) if (s.indexOf(i) === 2) i];
 
 // changing track
 function change_track(song_info) {
@@ -409,6 +409,6 @@ $('audio').on("pause", function() {
 // searchbar submit
 $("#songsearch_form").submit(function () { return change_track(songlist[findgame(song_searchbar.val_func(), songlist)]); });
 // autocomplete
-$("#songsearch").autocomplete({ source: gamenames });
+// $("#songsearch").autocomplete({ source: gamenames });
 
 // clicking second play on a different song while other song is currently playing shouldn't change button
