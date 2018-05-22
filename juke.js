d9834e4f3b99f42278707199b1a4845bf2a59e40
JamesS <jsibert@basecampcodingacademy.org>
2018-05-22 17:53:07 -0500

    Adding new version of songlist

--- juke.js
+++ juke.js
@@ -12,7 +12,7 @@ songlist = [["Main Theme (Super Mario Bros.)", "mario_theme", "Super Mario Bros.
             ["Gloomy Manor", "gloomy", "Luigi\'s Mansion: Dark Moon"],
             ["Rainbow Road", "rainbow", "Mario Kart 64"],
             ["Tick Tock Clock Remix", "tick_tock", "Mario Kart 8"],
-            ["Beware the Forest Mushrooms", "forest", "Super Mario RPG"],
+            ["Beware the Forest Mushrooms", "forest", "Super Mario RPG: Legend of the Seven Stars"],
             ["Come On!", "come_on", "Mario & Luigi: Superstar Saga"],
             ["Final Battle", "shroob", "Mario & Luigi: Partners in Time"],
             ["Grand Finale", "grand", "Mario & Luigi: Bowser\'s Inside Story"],
@@ -274,8 +274,442 @@ songlist = [["Main Theme (Super Mario Bros.)", "mario_theme", "Super Mario Bros.
             ["Nappa\'s Theme", "nappa", "Dragon Ball FighterZ"],
             ["Frieza\'s Theme", "frieza", "Dragon Ball FighterZ"],
             ["Hit\'s Theme", "dbfzhit", "Dragon Ball FighterZ"]]
-// 255 songs
-// Clean up and make into objects and keys
+
+// new version of song list, 124 games, 255 songs
+var jukebox = {
+  "Animal Crossing": [
+    ["Title (Animal Crossing)", "an_cross"]
+  ],
+  "Assassin\'s Creed 2": [
+    ["Venice Rooftops", "ac2_vr"],
+    ["Ezio\'s Family", "ac2_ef"]
+  ],
+  "Banjo-Kazooie": [
+    ["Click Clock Wood (Spring)", "ccspring_bk"],
+    ["Treasure Trove Cove", "ttcove_bk"],
+    ["Mad Monster Mansion", "mmm_bk"]
+  ],
+  "Batman: Arkham City": [
+    ["Main Theme (Batman: Arkham City)", "batman"]
+  ],
+  "Bayonetta": [
+    ["Let\'s Dance, Boys!", "bayonetta"]
+  ],
+  "Bayonetta 2": [
+    ["Tomorrow is Mine", "tomorrow_mine"]
+  ],
+  "Call of Duty: WWII": [
+    ["A Brotherhood of Heroes", "cod_boh"]
+  ],
+  "Cave Story+": [
+    ["Last Battle", "cave_story"]
+  ],
+  "Chibi-Robo!": [
+    ["Midday Majesty", "midday"],
+    ["Drake Redcrest Theme", "drake_redcrest"],
+    ["UFO Visitation", "ufo_visit"]
+  ],
+  "Crash Bandicoot": [
+    ["Main Theme (Crash Bandicoot)", "cb1_theme"]
+  ],
+  "Crypt of the Necrodancer": [
+    ["Mausoleum Mash (Shopkeeper)", "crypt"]
+  ],
+  "Devil May Cry 3": [
+    ["Devils Never Cry", "devilsnevercry"]
+  ],
+  "DISSIDIA FINAL FANTASY -Arcade-": [
+    ["Massive Explosion", "dissidia"]
+  ],
+  "Doki Doki Literature Club!": [
+    ["Your Reality", "ddlc_reality"]
+  ],
+  "Donkey Kong Country: Tropical Freeze": [
+    ["Big Top Bop", "big_top"]
+  ],
+  "DOOM": [
+    ["E1M1 - At Doom\'s Gate", "doom_theme"]
+  ],
+  "Dragon Ball Z: Budokai 3": [
+    ["Opening", "budokai"]
+  ],
+  "Dragon Ball Z: Budokai Tenkaichi 3": [
+    ["Super Survivor (Instrumental)", "super_survivor"]
+  ],
+  "Dragon Ball FighterZ": [
+    ["Future Trunks\'s Theme", "dbfztrunks"],
+    ["Nappa\'s Theme", "nappa"],
+    ["Frieza\'s Theme", "frieza"],
+    ["Hit\'s Theme", "dbfzhit"]
+  ],
+  "Drawn to Life": [
+    ["The End", "the_end"]
+  ],
+  "Drawn to Life: The Next Chapter": [
+    ["Real Life", "real_life"]
+  ],
+  "DuckTales": [
+    ["The Moon", "ducktales_moon"]
+  ],
+  "Earthbound": [
+    ["Onett", "onett_eb"],
+    ["Snowman", "snowman_eb"],
+    ["Smiles and Tears", "smiles_tears"]
+  ],
+  "Elder Scrolls V: Skyrim": [
+    ["Main Theme (Skyrim)", "skyrim"]
+  ],
+  "Fallout 3": [
+    ["Butcher Pete", "butcher_pete"]
+  ],
+  "Fallout New Vegas": [
+    ["Jingle Jangle Jingle", "jingle"]
+  ],
+  "FEZ": [
+    ["Compass", "fez_compass"]
+  ],
+  "Final Fantasy VII": [
+    ["Opening", "ff7opening"],
+    ["Aerith\'s Theme", "aerith"],
+    ["One Winged Angel", "onewinged"]
+  ],
+  "Final Fantasy X": [
+    ["Battle Theme", "battle_ffx"],
+    ["Otherworld", "otherworld_ffx"]
+  ],
+  "Final Fantasy XIII": [
+    ["Blinded by Light", "blinded_light"]
+  ],
+  "Final Fantasy XV": [
+    ["Apocalypsis Aquarius", "aquarius"],
+    ["Hammerhead - The Last Bastion", "last_bastion"],
+    ["Somnus", "somnus"]
+  ],
+  "Fire Emblem Awakening": [
+    ["Id (Purpose)", "purpose"]
+  ],
+  "Fire Emblem Fates": [
+    ["Lost in Thoughts All Alone", "lost_thoughts"],
+    ["Thorn in You (Roar)", "thorn"],
+    ["Perplexing Dreams", "perp_dream"],
+    ["A Dark Fall", "dark_fall"]
+  ],
+  "Gears of War 3": [
+    ["Mad World (Instrumental)", "madworld"]
+  ],
+  "Halo": [
+    ["Main Theme (Halo)", "halo"]
+  ],
+  "Halo 2: Anniversary Edition": [
+    ["Follow in Flight", "flight"],
+    ["Breaking the Covenant", "covenant"]
+  ],
+  "Jet Set Radio Future": [
+    ["Concept of Love", "concept"]
+  ],
+  "Kid Icarus Uprising": [
+    ["Magnus\'s Theme", "magnus"],
+    ["Dark Pit\'s Theme", "dark_pit"],
+    ["Lightning Battle", "lightning"]
+  ],
+  "Kingdom Hearts": [
+    ["Simple and Clean", "simple"],
+    ["Hikari Orchestra", "kh1_open"],
+    ["Night of Fate", "night_fate"],
+    ["Traverse Town", "traverse"],
+    ["Guardando nel Buio", "ansem"]
+  ],
+  "Kingdom Hearts 0.2 -A Fragmentary Passage-": [
+    ["Simple and Clean -Ray of Hope MIX-", "rayofhope"],
+    ["The Secret Whispers", "secret_whispers"],
+    ["Wave of Darkness 1", "dark_wave_kh", " 0.2"]
+  ],
+  "Kingdom Hearts 2": [
+    ["Dearly Beloved (KH2 Ver.)", "dearly_beloved2"],
+    ["Sanctuary", "sanctuary"],
+    ["Passion Orchestra",  "kh2_open"],
+    ["Roxas\'s Theme", "roxas"],
+    ["The 13th Struggle", "struggle"],
+    ["Sacred Moon", "sacred_moon"],
+    ["Fate of the Unknown", "fate_unknown"]
+  ],
+  "Kingdom Hearts 358/2 Days": [
+    ["Xion\'s Theme", "xion"],
+    ["Vector to the Heavens", "vector"],
+    ["Riku Battle", "riku"]
+  ],
+  "Kingdom Hearts Birth by Sleep": [
+    ["Dearly Beloved (BBS Ver.)", "dearly_beloved"],
+    ["Aqua\'s Theme", "aqua"],
+    ["Ventus", "ventus_bbs"],
+    ["Dismiss", "dismiss_kh"]
+  ],
+  "Kingdom Hearts Dream Drop Distance": [
+    ["L\'Impeto Oscuro", "young_x"]
+  ],
+  "Kingdom Hearts Re:Chain of Memories": [
+    ["Naminé", "namine"],
+    ["Hollow Bastion", "rcom_hollow_bastion"],
+    ["Lord of the Castle", "lord_of_castle"]
+  ],
+  "Kingdom Hearts Union X": [
+    ["Dearly Beloved (KHUX Ver.)", "khux"]
+  ],
+  "Kirby 64: The Crystal Shards": [
+    ["Miracle Matter", "mira_matt"],
+    ["Zero Two", "zerotwo"]
+  ],
+  "Kirby Planet Robobot": [
+    ["VS Star Dream", "stardream"],
+    ["Virtual Space Manipulator", "star_dream_vsp"],
+    ["P-R-O-G-R-A-M", "program"]
+  ],
+  "Kirby Super Star": [
+    ["King Dedede\'s Theme", "king_dedede"],
+    ["Meta Knight\'s Revenge", "meta_knight"]
+  ],
+  "Kirby Triple Deluxe": [
+    ["Dirty & Beauty", "sectonia"],
+    ["Moonstruck Blossom", "moonstruck"]
+  ],
+  "Kirby\'s Adventure": [
+    ["Rainbow Resort", "rainbow_resort"]
+  ],
+  "Kirby\'s Dream Land": [
+    ["Green Greens", "greens"]
+  ],
+  "Kirby\'s Return to Dreamland": [
+    ["Cookie Country", "cookie_c"],
+    ["C-R-O-W-N-E-D", "crowned"]
+  ],
+  "Luigi\'s Mansion: Dark Moon": [
+    ["Gloomy Manor", "gloomy"]
+  ],
+  "Mario & Luigi: Bowser\'s Inside Story": [
+    ["Grand Finale", "grand"]
+  ],
+  "Mario & Luigi: Dream Team": [
+    ["Adventure\'s End", "end"]
+  ],
+  "Mario & Luigi: Paper Jam": [
+    ["Final Boss Phase 2", "doublebowser"]
+  ],
+  "Mario & Luigi: Partners in Time": [
+    ["Final Battle", "shroob"]
+  ],
+  "Mario & Luigi: Superstar Saga": [
+    ["Come On!", "come_on"]
+  ],
+  "Mario Kart 64": [
+    ["Rainbow Road", "rainbow"]
+  ],
+  "Mario Kart 8": [
+    ["Tick Tock Clock Remix", "tick_tock"]
+  ],
+  "Mega Man 2": [
+    ["Dr. Wily\'s Castle: Stage 1", "wily"]
+  ],
+  "Metroid": [
+    ["Brinstar", "brinstar"]
+  ],
+  "Metroid: Samus Returns": [
+    ["Surface of SR388", "sr388"],
+    ["Brinstar Red Soil", "redsoil"],
+    ["VS Proteus Ridley", "proteus"]
+  ],
+  "Minecraft": [
+    ["Haggstrom", "haggstrom"]
+  ],
+  "Minit": [
+    ["Minit\'s Awakening", "minit_awaken"],
+    ["Sabasaba Desert", "saba_des"],
+    ["Sword Factory", "minit_swordfac"]
+  ],
+  "Nicktoons: Battle for Volcano Island": [
+    ["Ancient\'s Peak", "nbfvi_ancientpeak"],
+    ["Final Boss Theme", "nbfvi_finalboss"]
+  ],
+  "Nicktoons: Globs of Doom": [
+    ["Zim\'s Town (1)", "ngod_zim"]
+  ],
+  "NieR": [
+    ["Song of the Ancients - Fate", "song_ancients"]
+  ],
+  "NieR: Automata": [
+    ["Birth of a Wish", "birth_wish"],
+    ["City Ruins (Rays of Light)", "city_ruins_nier"],
+    ["Amusement Park", "amusement"],
+    ["A Beautiful Song", "opera_nier"],
+    ["Copied City", "copied_city"],
+    ["The Weight of the World", "weight_of_the_world"]
+  ],
+  "Night in the Woods": [
+    ["Title (Night in the Woods)", "nitw_title"],
+    ["Weird Autumn", "nitw_wa"],
+    ["Fireflies on the Porch", "nitw_fotp"]
+  ],
+  "Paper Mario: The Thousand Year Door": [
+    ["Rogueport", "rogueport"],
+    ["Battle Theme", "ttydbattle"],
+    ["Macho Grubba Battle", "grubba"],
+    ["Sadness and Happiness", "sad_happy_ttyd"],
+    ["Excess Express at Dusk", "at_dusk"],
+    ["Excess Express at Night", "at_night"],
+    ["X-Naut Fortress", "xnaut"]
+  ],
+  "Persona 3": [
+    ["Battle for Everyone\'s Souls", "persona3"]
+  ],
+  "Plants vs Zombies": [
+    ["Dr. Zomboss", "pvz"],
+    ["Zombies on your Lawn", "zombie_lawn"]
+  ],
+  "Playerunknown's Battlegrounds": [
+    ["Main Menu", "pubg_mm"]
+  ],
+  "Pokemon Omega Ruby & Alpha Sapphire": [
+    ["Battle Lorekeeper Zinnia", "zinnia"]
+  ],
+  "Pokemon Ultra Sun & Ultra Moon": [
+    ["Ultra Necrozma", "necrozma"]
+  ],
+  "Pokemon X & Y": [
+    ["Lumiose City", "lumiose"],
+    ["Battle Champion", "champxy"]
+  ],
+  "Portal": [
+    ["Still Alive", "still_alive"]
+  ],
+  "Super Mario 3D World": [
+    ["World Bowser", "world_bowser"]
+  ],
+  "Super Mario 64": [
+    ["Main Theme (Super Mario 64)", "mario_64"]
+  ],
+  "Super Mario Bros.": [
+    ["Main Theme (Super Mario Bros.)", "mario_theme"],
+    ["Underground Theme", "underground"],
+    ["Underwater Theme", "underwater"]
+  ],
+  "Super Mario Galaxy": [
+    ["Comet Observatory", "observ"],
+    ["Gusty Garden Galaxy", "gusty"]
+  ],
+  "Super Mario Odyssey": [
+    ["Jump Up Superstar!", "jump_superstar"],
+    ["Break Free", "break_free"]
+  ],
+  "Super Mario RPG: Legend of the Seven Stars": [
+    ["Beware the Forest Mushrooms", "forest"]
+  ],
+  "Super Mario Sunshine": [
+    ["Ricco Harbor", "ricco_harbor"]
+  ],
+  "Super Mario World": [
+    ["Overworld", "smw_ow"]
+  ],
+  "Super Paper Mario": [
+    ["Flipside", "flipside"],
+    ["Lineland Road", "lineland"],
+    ["Merlee\'s Mansion", "merlee"],
+    ["Sammer\'s Kingdom", "sammers"],
+    ["Champion of Destruction", "champ_destruction"],
+    ["Bounding Through Time", "bound_time"],
+    ["The Ultimate Show", "spm_ultimate"],
+    ["Proof of Existence", "proof_exist"]
+  ],
+  "Super Smash Bros.": [
+    ["Meta Crystal", "metal_ssb"]
+  ],
+  "Super Smash Bros. Brawl": [
+    ["Main Theme (Luigi\'s Mansion)", "luigis_mansion"]
+  ],
+  "Super Smash Bros. Melee": [
+    ["Fountain of Dreams", "fountain_dreams_ssbm"],
+    ["Fire Emblem", "melee_fe"]
+  ],
+  "Super Smash Land": [
+    ["Tower of Heaven", "tower_heaven"]
+  ],
+  "The Legend of Zelda": [
+    ["Main Theme (The Legend of Zelda)", "zelda_intro"]
+  ],
+  "The Legend of Zelda: A Link to the Past": [
+    ["The Dark World", "dark_world"]
+  ],
+  "The Legend of Zelda: Breath of the Wild": [
+    ["Main Theme (Breath of the Wild)", "breath_wild"],
+    ["Hyrule Castle (Outside w/ enemies)", "botw_castle"],
+    ["The Divine Trial (Final Battle)", "botw_divine"]
+
+  ],
+  "The Legend of Zelda: Majora\'s Mask": [
+    ["Song of Healing", "healing"]
+  ],
+  "The Legend of Zelda: Ocarina of Time": [
+    ["Title Theme", "title"],
+    ["Zelda\'s Lullaby", "lullaby"],
+    ["Song of Storms", "storms"],
+    ["Gerudo Valley", "gerudo"]
+  ],
+  "The Legend of Zelda: Skyward Sword": [
+    ["Ballad of the Goddess", "ballad"],
+    ["Skyloft", "skyloft"],
+    ["Fi\'s Theme", "fi"]
+  ],
+  "The Legend of Zelda: Twilight Princess": [
+    ["Hyrule Field", "hyrule_field"],
+    ["Midna\'s Lament", "midna_lament"]
+  ],
+  "The Legend of Zelda: The Wind Waker": [
+    ["The Great Sea", "greatsea"],
+    ["Dragon Roost Island", "dragonroost"]
+  ],
+  "The World Ends With You": [
+    ["Calling", "calling"],
+    ["Long Dream", "long_dream"],
+    ["Three Minutes Clapping", "three_minutes_clapping"],
+    ["Detonation", "detonation"],
+    ["Game Over", "game_over"],
+    ["Emptiness and", "emptiness_and"],
+    ["Lullaby for You", "lullaby_for_you"]
+  ],
+  "Touhou": [
+    ["UN Owen Was Her?", "un_owen"],
+    ["Necrofantasia", "necrof"]
+  ],
+  "Undertale": [
+    ["Your Best Friend", "best_friend"],
+    ["Undertale", "undertale"],
+    ["Heartache", "heartache"],
+    ["sans.", "sans"],
+    ["Snowdin Town", "snowdin"],
+    ["Bonetrousle", "bonetrousle"],
+    ["Waterfall", "waterfall"],
+    ["Temmie Village", "tem"],
+    ["Spear of Justice", "spear_justice"],
+    ["Alphys", "alphys"],
+    ["Another Medium", "medium"],
+    ["Spider Dance", "spider_dance"],
+    ["The Core", "core"],
+    ["Metal Crusher", "m_crusher"],
+    ["Death by Glamour", "death_by_glamour"],
+    ["Bergentrückung / ASGORE", "asgore"],
+    ["Finale", "ut_finale"],
+    ["Amalgam", "amalgam"],
+    ["Hopes and Dreams", "hopes_and_dreams"],
+    ["SAVE the World", "save_world"],
+    ["His Theme", "his_theme"],
+    ["Battle Against a True Hero", "battle_against"],
+    ["Megalovania", "megalovania"]
+  ],
+  "VVVVVV": [
+    ["Positive Force", "vvvvvv"]
+  ],
+  "Xenoblade Chronicles": [
+    ["Gaur Plains", "gaur"]
+  ]
+}
 
 var song_searchbar = {
     searchitem: null,
