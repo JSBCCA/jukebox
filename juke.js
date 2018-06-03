3f4976be71997ce48296209fb74e6d79859808c4
JamesS <jsibert@basecampcodingacademy.org>
2018-06-03 18:21:32 -0500

    Testing findsearch

--- juke.js
+++ juke.js
@@ -568,19 +568,21 @@ function pauseplay() {
 
 // findsearch(searched item, jukebox, current location) finds [game, song] indices in jukebox
 function findsearch(searched, jbx, curr_loc) {
-  var gameindex, gamename;
-  if ($.inArray(searched, Object.keys(jbx)) !== -1) {
-    // if game in jukebox, return [game index, 0]
-    return [$.inArray(searched, Object.keys(jbx)), 0];
-  } else if ($.inArray(searched, list_of_search_suggestions(jbx)) !== -1) {
-    // if song in jukebox, return [game index, song index]
-    for (gameindex in Object.keys(jbx)) { 
-      gamename = Object.keys(jbx)[gameindex];
-      if ($.inArray(searched, jbx[gamename]) !== -1) {
-        return [gameindex, $.inArray(searched, jbx[gamename])]
-      }
-    };
-  } else { return curr_loc; }
+//   var gameindex, gamename;
+//   if ($.inArray(searched, Object.keys(jbx)) !== -1) {
+//     // if game in jukebox, return [game index, 0]
+//     return [$.inArray(searched, Object.keys(jbx)), 0];
+//   } else if ($.inArray(searched, list_of_search_suggestions(jbx)) !== -1) {
+//     // if song in jukebox, return [game index, song index]
+//     for (gameindex in Object.keys(jbx)) { 
+//       gamename = Object.keys(jbx)[gameindex];
+//       if ($.inArray(searched, jbx[gamename]) !== -1) {
+//         return [gameindex, $.inArray(searched, jbx[gamename])]
+//       }
+//     };
+//   } else { 
+    return curr_loc; 
+//   }
 };
 
 // moving right
